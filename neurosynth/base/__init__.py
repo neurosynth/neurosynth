e2f0da294ec5f0b6c6d203caf288c29c16b4d129
Yaroslav Halchenko <debian@onerussian.com>
2013-05-26 18:47:30 -0400

    ENH: Demand nibabel >= 1.3 (Close #5)
    Check is done in neurosynth.base.__init__

--- neurosynth/base/__init__.py
+++ neurosynth/base/__init__.py
@@ -2,3 +2,9 @@
 # ex: set sts=2 ts=2 sw=2 et:
 
 __all__ = ['dataset', 'mask', 'imageutils', 'mappable', 'transformations', 'lexparser']
+
+# Verify nibabel version right here since nothing would work
+import nibabel as nib
+from distutils.version import LooseVersion
+if LooseVersion(nib.__version__) < LooseVersion("1.3"):
+    raise ImportError("Neurosynth requires nibabel >= 1.3")
