9d4651527af63c2be4202ea0b17e4c69150273e3
tyarkoni <tyarkoni@gmail.com>
2014-04-05 01:40:01 -0400

    removed unused files

--- neurosynth/analysis/misc.py
+++
@@ -1,3 +0,0 @@
-# emacs: -*- mode: python-mode; py-indent-offset: 2; tab-width: 2; indent-tabs-mode: nil -*-
-# ex: set sts=2 ts=2 sw=2 et:
-""" Miscellaneous analysis methods. """
--- neurosynth/analysis/ml.py
+++
@@ -1,1 +0,0 @@
-""" Machine-learning-related methods. """
