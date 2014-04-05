36976caa3162f7e02e2061cae1cc0dbdb23f37dc
tyarkoni <tyarkoni@gmail.com>
2014-04-04 20:24:20 -0600

    updated list of analysis modules

--- neurosynth/analysis/__init__.py
+++ neurosynth/analysis/__init__.py
@@ -1,4 +1,4 @@
 # emacs: -*- mode: python-mode; py-indent-offset: 2; tab-width: 2; indent-tabs-mode: nil -*-
 # ex: set sts=2 ts=2 sw=2 et:
 
-__all__ = ['classify', 'decode', 'meta', 'ml', 'network', 'plotutils', 'reduce', 'stats']
+__all__ = ['classify', 'cluster', 'decode', 'meta', 'network', 'reduce', 'stats']
