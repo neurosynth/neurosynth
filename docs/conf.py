8f655f32cb4fdd7a1ecfafd9047794a3a7f2b531
tyarkoni <tyarkoni@gmail.com>
2014-12-30 22:58:05 -0600

    relative import path

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@
 
 import sys
 import os
-
+sys.path.insert(0, os.path.abspath("../neurosynth/analysis"))
+sys.path.insert(0, os.path.abspath("../neurosynth/base"))
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
